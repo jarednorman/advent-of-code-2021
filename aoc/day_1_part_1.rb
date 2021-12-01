a1698c7841904ca886fb51a873e8a6fe15ae6cc2
Jared Norman <jared@super.gd>
2021-11-30 21:08:24 -0800

    Day 1 part 1

--- aoc/day_1_part_1.rb
+++ aoc/day_1_part_1.rb
@@ -1,4 +1,11 @@
 class AoC::Day1Part1
+  def initialize(input: File.read("data/day1.txt"))
+    @numbers = input.split.map(&:to_i)
+  end
+
   def solution
+    @numbers.each_cons(2).count do |a, b|
+      a < b
+    end
   end
 end
--- spec/aoc/day_1_part_1_spec.rb
+++ spec/aoc/day_1_part_1_spec.rb
@@ -2,10 +2,25 @@ require "spec_helper"
 
 RSpec.describe AoC::Day1Part1 do
   describe "#solution" do
-    subject { described_class.new.solution }
+    subject { described_class.new(input: test_input).solution }
+
+    let(:test_input) {
+      <<~INPUT
+        199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
+      INPUT
+    }
 
     it "works" do
-      expect(subject).to eq 2
+      expect(subject).to eq 7
     end
   end
 end
