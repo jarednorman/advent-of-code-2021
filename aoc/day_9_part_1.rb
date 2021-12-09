49609caa8d3a5637bab82ad8272c52df1ccfc8d3
Jared Norman <jared@super.gd>
2021-12-08 21:16:34 -0800

    Day 9 part 1

--- aoc/day_9_part_1.rb
+++ aoc/day_9_part_1.rb
@@ -1,7 +1,56 @@
 class AoC::Day9Part1
+  class HeightMap
+    def initialize(data)
+      @data = data
+    end
+
+    def width
+      @data.length
+    end
+
+    def height
+      @data.first.length
+    end
+
+    def each_cell
+      @data.each_with_index { |row, x|
+        row.each_with_index { |cell, y|
+          yield cell, x, y
+        }
+      }
+    end
+
+    def neighbours(x, y)
+      n = []
+
+      n << cell(x - 1, y) if x > 0
+      n << cell(x + 1, y) if x < width - 1
+      n << cell(x, y - 1) if y > 0
+      n << cell(x, y + 1) if y < height - 1
+
+      n
+    end
+
+    def cell(x, y)
+      @data[x][y]
+    end
+  end
+
   def initialize(input = File.read("data/day9.txt"))
+    @map = HeightMap.new(
+      input.split("\n").map { |line| line.split("").map(&:to_i) }.transpose
+    )
   end
 
   def solution
+    low_points = []
+
+    @map.each_cell { |cell, x, y|
+      if @map.neighbours(x, y).all? { |z| z > cell }
+        low_points << cell
+      end
+    }
+
+    low_points.sum + low_points.length
   end
 end
---
+++ spec/aoc/day_9_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+RSpec.describe "Day 9" do
+  let(:input) { <<~INPUT }
+    2199943210
+    3987894921
+    9856789892
+    8767896789
+    9899965678
+  INPUT
+
+  describe "Part 1", :focus do
+    subject { AoC::Day9Part1.new(input).solution }
+
+    it { is_expected.to eq 15 }
+  end
+
+  describe "Part 2" do
+    subject { AoC::Day9Part1.new(input).solution }
+
+    it { is_expected.to eq nil }
+  end
+end
