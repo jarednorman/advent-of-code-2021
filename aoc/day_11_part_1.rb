875129cbd79eaa35d4e31215e513fef2acf679c7
Jared Norman <jared@super.gd>
2021-12-12 14:36:34 -0800

    Day 11 part 1

--- aoc/day_11_part_1.rb
+++ aoc/day_11_part_1.rb
@@ -1,7 +1,103 @@
 class AoC::Day11Part1
+  class Octopus
+    def initialize(power)
+      @power = power
+      @flashed = false
+    end
+
+    def power
+      @power
+    end
+
+    def increment
+      @power += 1
+    end
+
+    def flash
+      if !@flashed && @power > 9
+        @flashed = true
+      else
+        false
+      end
+    end
+
+    def reset
+      @flashed = false
+
+      @power = 0 if @power > 9
+    end
+  end
+
+  class Grid
+    def initialize(grid)
+      @octopi = grid.map { |column| column.map { |power| Octopus.new(power) } }
+    end
+
+    def step
+      @flashes = 0
+
+      @octopi.flatten.each(&:increment)
+
+      @octopi.each_with_index do |column, x|
+        column.each_with_index do |octopus, y|
+          flash(x, y)
+        end
+      end
+
+      @octopi.flatten.each(&:reset)
+
+      @flashes
+    end
+
+    def print
+      puts(@octopi.transpose.map { |row|
+        row.map(&:power).join('')
+      }.join("\n")+  "\n\n")
+    end
+
+    private
+
+    def flash(x, y)
+      return if x < 0 || y < 0
+      return if x >= @octopi.length || y >= @octopi.first.length
+
+      return unless @octopi[x][y].flash
+
+      @flashes += 1
+
+      [-1, 0, 1].each do |dx|
+        [-1, 0, 1].each do |dy|
+          next if [dx, dy] == [0, 0]
+
+          increment(x + dx, y + dy)
+          flash(    x + dx, y + dy)
+        end
+      end
+    end
+
+    def increment(x, y)
+      return if x < 0 || y < 0
+      return if x >= @octopi.length || y >= @octopi.first.length
+
+      @octopi[x][y].increment
+    end
+  end
+
   def initialize(input = File.read("data/day11.txt"))
+    @grid = Grid.new(input.split("\n").map { |line| line.split("").map(&:to_i) }.transpose)
   end
 
   def solution
+    @grid.print
+
+    100.times.sum { |n|
+      result = @grid.step
+
+      if n < 10
+        @grid.print
+      end
+
+      result
+    }
   end
 end
--- spec/aoc/day_11_spec.rb
+++ spec/aoc/day_11_spec.rb
@@ -4,10 +4,20 @@ RSpec.describe "Day 11" do
   subject { described_class.new(input).solution }
 
   let(:input) { <<~INPUT }
+    5483143223
+    2745854711
+    5264556173
+    6141336146
+    6357385478
+    4167524645
+    2176841721
+    6882881134
+    4846848554
+    5283751526
   INPUT
 
   describe AoC::Day11Part1 do
-    it { is_expected.to eq nil }
+    it { is_expected.to eq 1656 }
   end
 
   describe AoC::Day11Part2, :skip do
