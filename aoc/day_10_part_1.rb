30791f90bbf44c0901533dbdba5d2b2ff1a7ae5a
Jared Norman <jared@super.gd>
2021-12-12 13:47:32 -0800

    Day 10 part 1

--- aoc/day_10_part_1.rb
+++ aoc/day_10_part_1.rb
@@ -1,7 +1,51 @@
 class AoC::Day10Part1
+  class Line
+    PARENS = {
+      "(" => ")",
+      "[" => "]",
+      "{" => "}",
+      "<" => ">"
+    }
+
+    def initialize(chars)
+      stack = []
+      chars.each do |c|
+        if PARENS.keys.include? c
+          stack.push c
+        elsif PARENS.values.include? c
+          match = PARENS.fetch(stack.pop)
+
+          if match != c
+            @bad_char = c
+            break
+          end
+        else
+          raise "Unexpected char: #{c.inspect}"
+        end
+      end
+    end
+
+    def corrupted?
+      !!@bad_char
+    end
+
+    def syntax_error_score
+      {
+        ")" => 3,
+        "]" => 57,
+        "}" => 1197,
+        ">" => 25137
+      }[@bad_char]
+    end
+  end
+
   def initialize(input = File.read("data/day10.txt"))
+    @lines = input.split("\n").map { |line| line.split('') }.map { |line|
+      Line.new(line)
+    }
   end
 
   def solution
+    @lines.select(&:corrupted?).sum(&:syntax_error_score)
   end
 end
--- spec/aoc/day_10_spec.rb
+++ spec/aoc/day_10_spec.rb
@@ -4,13 +4,47 @@ RSpec.describe "Day 10" do
   subject { described_class.new(input).solution }
 
   let(:input) { <<~INPUT }
+    [({(<(())[]>[[{[]{<()<>>
+    [(()[<>])]({[<{<<[]>>(
+    {([(<{}[<>[]}>{[]{[(<()>
+    (((({<>}<{<{<>}{[]{[]{}
+    [[<[([]))<([[{}[[()]]]
+    [{[{({}]{}}([{[{{{}}([]
+    {<[[]]>}<{[{[{[]{()[[[]
+    [<(<(<(<{}))><([]([]()
+    <{([([[(<>()){}]>(<<{{
+    <{([{{}}[<[[[<>{}]]]>[]]
   INPUT
 
   describe AoC::Day10Part1 do
-    it { is_expected.to eq nil }
+    it { is_expected.to eq 26397 }
   end
 
   describe AoC::Day10Part2, :skip do
     it { is_expected.to eq nil }
+  end
+
+  describe AoC::Day10Part1::Line do
+    subject { described_class.new(chars) }
+
+    context "erroneous }" do
+      let(:chars) { <<~CHARS.split('') }
+        {([(<{}[<>[]}>{[]{[(<()>
+      CHARS
+
+      it { is_expected.to be_corrupted }
+
+      its(:syntax_error_score) { is_expected.to eq 1197 }
+    end
+
+    context "erroneous )" do
+      let(:chars) { <<~CHARS.split('') }
+        [<(<(<(<{}))><([]([]()
+      CHARS
+
+      it { is_expected.to be_corrupted }
+
+      its(:syntax_error_score) { is_expected.to eq 3 }
+    end
   end
 end
