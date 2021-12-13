333ce8ee83667f8010dade4d3de197d4263db588
Jared Norman <jared@super.gd>
2021-12-12 19:10:06 -0800

    Day 12 part 1

--- aoc/day_12_part_1.rb
+++ aoc/day_12_part_1.rb
@@ -1,7 +1,30 @@
 class AoC::Day12Part1
   def initialize(input = File.read("data/day12.txt"))
+    @map = Hash.new { |h, k| h[k] = [] }
+
+    input.split("\n").each do |pair|
+      a, b = *pair.split("-")
+
+      @map[a] << b
+      @map[b] << a
+    end
   end
 
   def solution
+    path = ["start"]
+
+    count_paths(path)
+  end
+
+  private
+
+  def count_paths(path)
+    return 1 if path.last == "end"
+
+    @map[path.last].reject do |adjacent|
+      adjacent.downcase == adjacent && path.include?(adjacent)
+    end.sum do |adjacent|
+      count_paths(path + [adjacent])
+    end
   end
 end
--- spec/aoc/day_12_spec.rb
+++ spec/aoc/day_12_spec.rb
@@ -4,10 +4,17 @@ RSpec.describe "Day 12" do
   subject { described_class.new(input).solution }
 
   let(:input) { <<~INPUT }
+    start-A
+    start-b
+    A-c
+    A-b
+    b-d
+    A-end
+    b-end
   INPUT
 
   describe AoC::Day12Part1 do
-    it { is_expected.to eq nil }
+    it { is_expected.to eq 10 }
   end
 
   describe AoC::Day12Part2, :skip do
