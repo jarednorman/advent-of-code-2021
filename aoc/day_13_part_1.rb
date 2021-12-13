c02ba4c4512d2799eaa5f1897209361d9f052e41
Jared Norman <jared@super.gd>
2021-12-12 21:19:13 -0800

    Day 13 part 1

--- aoc/day_13_part_1.rb
+++ aoc/day_13_part_1.rb
@@ -1,7 +1,48 @@
 class AoC::Day13Part1
+  class Paper < Struct.new(:dots)
+    def fold(x: nil, y: nil)
+      raise unless x || y
+
+      if y
+        self.class.new(
+          dots.map { |dot|
+            next dot if dot.last < y
+
+            [dot.first, y - (dot.last - y)]
+          }.uniq
+        )
+      else
+        self.class.new(
+          dots.map { |dot|
+            next dot if dot.first < x
+
+            [x - (dot.first - x), dot.last]
+          }.uniq
+        )
+      end
+    end
+  end
+
   def initialize(input = File.read("data/day13.txt"))
+    dots, folds = *input.split("\n\n")
+
+    @paper = Paper.new(dots.split("\n").map{|d|d.split(",").map(&:to_i)})
+
+    @folds = folds.split("\n").map { |f|
+      _, axis, z = */fold along (x|y)=(\d+)/.match(f)
+      [axis, z.to_i]
+    }
   end
 
   def solution
+    @paper =
+      case @folds.first.first
+      when "x" then @paper.fold(x: @folds.first.last)
+      when "y" then @paper.fold(y: @folds.first.last)
+      else
+        raise "wat"
+      end
+
+    @paper.dots.count
   end
 end
--- spec/aoc/day_13_spec.rb
+++ spec/aoc/day_13_spec.rb
@@ -4,10 +4,31 @@ RSpec.describe "Day 13" do
   subject { described_class.new(input).solution }
 
   let(:input) { <<~INPUT }
+    6,10
+    0,14
+    9,10
+    0,3
+    10,4
+    4,11
+    6,0
+    6,12
+    4,1
+    0,13
+    10,12
+    3,4
+    3,0
+    8,4
+    1,10
+    2,14
+    8,10
+    9,0
+
+    fold along y=7
+    fold along x=5
   INPUT
 
   describe AoC::Day13Part1 do
-    it { is_expected.to eq nil }
+    it { is_expected.to eq 17 }
   end
 
   describe AoC::Day13Part2, :skip do
