3485cba70c4717cc5f65dcf9b24ecddf015fe7df
Jared Norman <jared@super.gd>
2021-12-16 17:59:32 -0800

    Day 15 part 1

--- aoc/day_15_part_1.rb
+++ aoc/day_15_part_1.rb
@@ -1,7 +1,45 @@
 class AoC::Day15Part1
   def initialize(input = File.read("data/day15.txt"))
+    @map = input.strip.split("\n").map { |l|
+      l.split('').map(&:to_i)
+    }.transpose
   end
 
   def solution
+    @mins = {}
+
+    min_path(width - 1, height - 1)
+  end
+
+  private
+
+  def min_path(x, y)
+    if (m = @mins[[x, y]])
+      return m
+    end
+    return 0 if [x, y] == [0, 0]
+
+    @mins[[x,y]] = neighbours(x, y)
+      .map { |x,y| min_path(x, y) }
+      .min + at(x, y)
+  end
+
+  def neighbours(x, y)
+    [
+      ([x, y - 1] if y > 0),
+      ([x - 1, y] if x > 0)
+    ].compact
+  end
+
+  def width
+    @width ||= @map.length
+  end
+
+  def height
+    @height ||= @map.first.length 
+  end
+
+  def at(x, y)
+    @map[x][y]
   end
 end
--- spec/aoc/day_15_spec.rb
+++ spec/aoc/day_15_spec.rb
@@ -4,10 +4,20 @@ RSpec.describe "Day 15" do
   subject { described_class.new(input).solution }
 
   let(:input) { <<~INPUT }
+    1163751742
+    1381373672
+    2136511328
+    3694931569
+    7463417111
+    1319128137
+    1359912421
+    3125421639
+    1293138521
+    2311944581
   INPUT
 
-  describe AoC::Day15Part1 do
-    it { is_expected.to eq nil }
+  describe AoC::Day15Part1, :focus do
+    it { is_expected.to eq 40 }
   end
 
   describe AoC::Day15Part2, :skip do
