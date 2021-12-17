c3db40c649a335969bb08f0ffc7651b0fbd4c1ee
Jared Norman <jared@super.gd>
2021-12-16 17:14:48 -0800

    Day 14 part 1

--- aoc/day_14_part_1.rb
+++ aoc/day_14_part_1.rb
@@ -1,7 +1,50 @@
 class AoC::Day14Part1
+  class Polymer
+    def initialize(template, rules)
+      @template = template.split('')
+      @rules = rules
+    end
+
+    def template
+      @template.join
+    end
+
+    def step
+      @template = @template.each_cons(2).each_with_index.map { |pair, i|
+        insertion = @rules[pair]
+
+        if i == 0
+          a, b = *pair
+          [a, insertion, b]
+        else
+          [insertion, pair.last]
+        end
+      }.flatten
+    end
+
+    def answer
+      counts = @template.tally.values
+      min = counts.min
+      max = counts.max
+
+      max - min
+    end
+  end
+
   def initialize(input = File.read("data/day14.txt"))
+    @template, @rules = *input.split("\n\n")
+
+    @rules = @rules.strip.split("\n").map { |line|
+      a, b = *line.split(" -> ")
+      [a.split(''), b]
+    }.to_h
+
+    @polymer = Polymer.new(@template, @rules)
   end
 
   def solution
+    10.times { @polymer.step }
+
+    @polymer.answer
   end
 end
--- spec/aoc/day_14_spec.rb
+++ spec/aoc/day_14_spec.rb
@@ -4,10 +4,28 @@ RSpec.describe "Day 14" do
   subject { described_class.new(input).solution }
 
   let(:input) { <<~INPUT }
+    NNCB
+
+    CH -> B
+    HH -> N
+    CB -> H
+    NH -> C
+    HB -> C
+    HC -> B
+    HN -> C
+    NN -> C
+    BH -> H
+    NC -> B
+    NB -> B
+    BN -> B
+    BB -> N
+    BC -> B
+    CC -> N
+    CN -> C
   INPUT
 
   describe AoC::Day14Part1 do
-    it { is_expected.to eq nil }
+    it { is_expected.to eq 1588 }
   end
 
   describe AoC::Day14Part2, :skip do
