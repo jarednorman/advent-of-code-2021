4dacdfe03f367b595c68d98ab95a4fe1bdc8c0a8
Jared Norman <jared@super.gd>
2021-12-18 15:39:54 -0800

    Day 16 part 1

--- aoc/day_16_part_1.rb
+++ aoc/day_16_part_1.rb
@@ -1,7 +1,88 @@
 class AoC::Day16Part1
+  HEX_MAP = {
+    "0" => %w(0 0 0 0),
+    "1" => %w(0 0 0 1),
+    "2" => %w(0 0 1 0),
+    "3" => %w(0 0 1 1),
+    "4" => %w(0 1 0 0),
+    "5" => %w(0 1 0 1),
+    "6" => %w(0 1 1 0),
+    "7" => %w(0 1 1 1),
+    "8" => %w(1 0 0 0),
+    "9" => %w(1 0 0 1),
+    "A" => %w(1 0 1 0),
+    "B" => %w(1 0 1 1),
+    "C" => %w(1 1 0 0),
+    "D" => %w(1 1 0 1),
+    "E" => %w(1 1 1 0),
+    "F" => %w(1 1 1 1)
+  }
+  class Packet
+    def initialize
+      self.subpackets = []
+    end
+
+    attr_accessor(
+      :type,
+      :version,
+      :length_type_id,
+      :length,
+      :subpacket_count,
+      :subpackets,
+      :value
+    )
+
+    def version_sum
+      version + subpackets.sum(&:version_sum)
+    end
+  end
+
   def initialize(input = File.read("data/day16.txt"))
+    @binary = input.split('').flat_map { |c| HEX_MAP[c] }
   end
 
   def solution
+    parse_packet(@binary).version_sum
+  end
+
+  private
+
+  def parse_packet(packet_data)
+    Packet.new.tap do |packet|
+      binding.pry if packet_data.nil?
+      packet.version = packet_data.shift(3).join.to_i(2)
+      packet.type = packet_data.shift(3).join.to_i(2)
+
+      if packet.type != 4
+        packet.length_type_id = packet_data.shift.to_i(2)
+
+        if packet.length_type_id == 0
+          packet.length = packet_data.shift(15).join.to_i(2)
+
+          subpacket_data = packet_data.shift(packet.length)
+
+          until subpacket_data.empty?
+            packet.subpackets << parse_packet(subpacket_data)
+          end
+        else
+          packet.subpacket_count = packet_data.shift(11).join.to_i(2)
+
+          until packet.subpackets.count == packet.subpacket_count
+            packet.subpackets << parse_packet(packet_data)
+          end
+        end
+      else
+        value = []
+
+        loop do
+          check, *chunk = packet_data.shift(5)
+          value += chunk
+
+          break if check == "0"
+        end
+
+        packet.value = value.join.to_i(2)
+      end
+    end
   end
 end
--- spec/aoc/day_16_spec.rb
+++ spec/aoc/day_16_spec.rb
@@ -3,11 +3,31 @@ require "spec_helper"
 RSpec.describe "Day 16" do
   subject { described_class.new(input).solution }
 
-  let(:input) { <<~INPUT }
-  INPUT
-
   describe AoC::Day16Part1 do
-    it { is_expected.to eq nil }
+    context "using the first test input" do
+      let(:input) { "8A004A801A8002F478" }
+
+      it { is_expected.to eq 16 }
+    end
+
+    context "using the second test input" do
+      let(:input) { "620080001611562C8802118E34" }
+
+
+      it { is_expected.to eq 12 }
+    end
+
+    context "using the third test input" do
+      let(:input) { "C0015000016115A2E0802F182340" }
+
+      it { is_expected.to eq 23 }
+    end
+
+    context "using the fourth test input" do
+      let(:input) { "A0016C880162017C3686B18A3D4780" }
+
+      it { is_expected.to eq 31 }
+    end
   end
 
   describe AoC::Day16Part2, :skip do
