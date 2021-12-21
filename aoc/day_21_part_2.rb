52ad39138e90344db43eafa49b1156162416671c
Jared Norman <jared@super.gd>
2021-12-21 15:54:31 -0800

    Day 21 part 2 except I roll three times

--- aoc/day_21_part_2.rb
+++ aoc/day_21_part_2.rb
@@ -9,6 +9,10 @@ class AoC::Day21Part2 < AoC::Day21Part1
     :p1_turn,
     keyword_init: true
   ) do
+    def done?
+      p1_win? || p2_win?
+    end
+
     def p1_win?
       p1_score >= SCORE_LIMIT
     end
@@ -19,32 +23,43 @@ class AoC::Day21Part2 < AoC::Day21Part1
 
     def next_states
       if p1_turn
-        [1, 2, 3].map do |roll|
-          new_p1_pos = (p1_pos + roll - 1) % 10 + 1
+        [1, 2, 3].flat_map do |roll_one|
+          [1, 2, 3].flat_map do |roll_two|
+            [1, 2, 3].map do |roll_three|
+              roll = roll_one + roll_two + roll_three
 
-          State.new(
-            p1_pos: new_p1_pos,
-            p1_score: p1_score + new_p1_pos,
+              new_p1_pos = (p1_pos + roll - 1) % 10 + 1
 
-            p2_pos: p2_pos,
-            p2_score: p2_score,
+              State.new(
+                p1_pos: new_p1_pos,
+                p1_score: p1_score + new_p1_pos,
 
-            p1_turn: false
-          )
+                p2_pos: p2_pos,
+                p2_score: p2_score,
+
+                p1_turn: false
+              )
+            end
+          end
         end
       else
-        [1, 2, 3].map do |roll|
-          new_p2_pos = (p2_pos + roll - 1) % 10 + 1
+        [1, 2, 3].flat_map do |roll_one|
+          [1, 2, 3].flat_map do |roll_two|
+            [1, 2, 3].map do |roll_three|
+              roll = roll_one + roll_two + roll_three
+              new_p2_pos = (p2_pos + roll - 1) % 10 + 1
 
-          State.new(
-            p1_pos: p1_pos,
-            p1_score: p1_score,
+              State.new(
+                p1_pos: p1_pos,
+                p1_score: p1_score,
 
-            p2_pos: new_p2_pos,
-            p2_score: p2_score + new_p2_pos,
+                p2_pos: new_p2_pos,
+                p2_score: p2_score + new_p2_pos,
 
-            p1_turn: true
-          )
+                p1_turn: true
+              )
+            end
+          end
         end
       end
     end
@@ -61,15 +76,32 @@ class AoC::Day21Part2 < AoC::Day21Part1
       p1_turn: true
     )
 
-    wins(initial).max
-  end
+    @done_states = Hash.new(0)
 
-  private
+    states = Hash.new(0)
+    states[initial] += 1
 
-  def wins(state)
-    return [1, 0] if state.p1_win?
-    return [0, 1] if state.p2_win?
+    loop do
+      break if states.empty?
 
-    state.next_states.map { |state| wins(state) }.transpose.map(&:sum)
+      new_states = Hash.new(0)
+
+      states.each { |state, count|
+        state.next_states.each do |next_state|
+          if next_state.done?
+            @done_states[next_state] += count
+          else
+            new_states[next_state] += count
+          end
+        end
+      }
+
+      states = new_states
+    end
+
+    [
+      @done_states.select{|k,v|k.p1_win?}.sum{|k,v| v },
+      @done_states.select{|k,v|k.p2_win?}.sum{|k,v| v }
+    ].max
   end
 end
