b21b749a815badd660fb9ab44e531e07e248d074
Jared Norman <jared@super.gd>
2021-12-21 14:43:37 -0800

    Day 21 part 1

--- aoc/day_21_part_1.rb
+++ aoc/day_21_part_1.rb
@@ -1,7 +1,53 @@
 class AoC::Day21Part1
+  class DeterministicDie
+    def initialize
+      @roll_count = 0
+    end
+
+    def roll
+      @roll_count += 1
+
+      (@roll_count - 1) % 100 + 1
+    end
+
+    def roll_count
+      @roll_count
+    end
+  end
+
   def initialize(input = File.read("data/day21.txt"))
+    @p1_pos, @p2_pos = *input.strip.split("\n").map { |line|
+      line.split(" ").last.to_i
+    }
+
+    @p1_pos -= 1
+    @p2_pos -= 1
+
+    @p1_score = 0
+    @p2_score = 0
+
+    @die = DeterministicDie.new
   end
 
   def solution
+    loop do
+      move = @die.roll + @die.roll + @die.roll
+
+      @p1_pos = (@p1_pos + move) % 10
+      @p1_score += @p1_pos + 1
+
+      if @p1_score >= 1000
+        return @p2_score * @die.roll_count
+      end
+
+      move = @die.roll + @die.roll + @die.roll
+
+      @p2_pos = (@p2_pos + move) % 10
+      @p2_score += @p2_pos + 1
+
+      if @p2_score >= 1000
+        return @p1_score * @die.roll_count
+      end
+    end
   end
 end
--- spec/aoc/day_21_spec.rb
+++ spec/aoc/day_21_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe "Day 21" do
   subject { described_class.new(input).solution }
 
   let(:input) { <<~INPUT }
+    Player 1 starting position: 4
+    Player 2 starting position: 8
   INPUT
 
-  describe AoC::Day21Part1 do
-    it { is_expected.to eq nil }
+  describe AoC::Day21Part1, :focus do
+    it { is_expected.to eq 739785 }
   end
 
   describe AoC::Day21Part2, :skip do
