775020f3bde4300d6929c1025b63f0db088331a0
Jared Norman <jared@super.gd>
2021-12-28 17:08:39 -0800

    Day 25 complete

--- aoc/day_25_part_1.rb
+++ aoc/day_25_part_1.rb
@@ -1,7 +1,80 @@
 class AoC::Day25Part1
   def initialize(input = File.read("data/day25.txt"))
+    @state = input.strip.split("\n").each_with_index.flat_map { |line, y|
+      line.split('').each_with_index.map { |c, x|
+        [[x, y], c] unless c == "."
+      }.compact
+    }.to_h
+
+    @height = @state.keys.map(&:last).max + 1
+    @width = @state.keys.map(&:first).max + 1
   end
 
   def solution
+    steps = 0
+
+    state = @state
+    loop do
+      steps += 1
+      new_state = advance(state)
+
+      break if new_state == state
+
+      state = new_state
+    end
+
+    steps
+  end
+
+  private
+
+  def print(state)
+    puts "\n"
+    (0...@height).each do |y|
+      puts((0...@width).map { |x| 
+        state[[x, y]] || "."
+      }.join)
+    end
+  end
+
+  def advance(state)
+    new_state = {}
+
+    # East first
+    state.each do |(x, y), c|
+      if c == ">"
+        dx = x + 1
+        dx = 0 if dx >= @width
+
+        if state[[dx, y]]
+          new_state[[x, y]] = c
+        else
+          new_state[[dx, y]] = c
+        end
+      else
+        new_state[[x, y]] = c
+      end
+    end
+
+    state = new_state
+    new_state = {}
+
+    # Then South
+    state.each do |(x, y), c|
+      if c == "v"
+        dy = y + 1
+        dy = 0 if dy >= @height
+
+        if state[[x, dy]]
+          new_state[[x, y]] = c
+        else
+          new_state[[x, dy]] = c
+        end
+      else
+        new_state[[x, y]] = c
+      end
+    end
+
+    new_state
   end
 end
--- spec/aoc/day_25_spec.rb
+++ spec/aoc/day_25_spec.rb
@@ -4,10 +4,19 @@ RSpec.describe "Day 25" do
   subject { described_class.new(input).solution }
 
   let(:input) { <<~INPUT }
+    v...>>.vv>
+    .vv>>.vv..
+    >>.>v>...v
+    >>v>>.>.v.
+    v>v.vv.v..
+    >.>>..v...
+    .vv..>.>v.
+    v.v..>>v.v
+    ....v..v.>
   INPUT
 
-  describe AoC::Day25Part1 do
-    it { is_expected.to eq nil }
+  describe AoC::Day25Part1, :focus do
+    it { is_expected.to eq 58 }
   end
 
   describe AoC::Day25Part2, :skip do
